d209d429f6a4afba1f1b4310c9a2d1b31d85c1f0
harsha614261 <harshagokulsai00@gmail.com>
2022-10-10 15:10:06 +0530

    sample text file

--- app/code/Harsha/CustomCms/Block/Banner.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Harsha\CustomCms\Block;
-class Banner extends \Magento\Framework\View\Element\Template
-{
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
-    {
-        return parent::__construct($context);
-    }
-}
